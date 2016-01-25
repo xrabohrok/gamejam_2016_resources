ad9dcf499de3021de1e7d96508612018ad4e946c
Mr. Black <westin007@gmail.com>
2016-01-24 19:31:35 -0600

    Stripped down version of the dialogue box, doesn't work right - Mr.Black

---
+++ Assets/code/DialogueBox/DialogueBox.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class DialogueBox : MonoBehaviour {
+    public float buttonWidthPercent = .20f;
+    public float buttonHeightPercent = .05f;
+    public float verticalButtonDisp = .6f;
+    public float horizontalButtonDisp = .5f;
+
+
+	public Texture2D background;
+	public GUISkin guiSkin;
+
+	private int originalFontSize = 32;
+	private float nativeResY = 530.0f;
+
+    void OnGUI()
+    {
+		GUI.skin = guiSkin;
+		GUIStyle backgroundStyle = new GUIStyle();
+		backgroundStyle.normal.background = background;
+		GUI.Label(new Rect(0,0,Screen.width, Screen.height), "", backgroundStyle);
+		GUIStyle menuButton = new GUIStyle(guiSkin.button);
+		menuButton.fontSize = (int) (originalFontSize * (Screen.height/nativeResY));
+
+
+        float buttonWidth = Screen.width * buttonWidthPercent;
+        float buttonHeight = Screen.height * buttonHeightPercent;
+
+        if (GUI.Button(new Rect(Screen.width * horizontalButtonDisp - buttonWidth/2, 
+		                        Screen.height * verticalButtonDisp + buttonHeight * 1 , 
+		                        buttonWidth , buttonHeight), "New Game", menuButton))
+        {
+            //advance
+        }
+
+
+    }
+
+
+
+
+
+
+}
